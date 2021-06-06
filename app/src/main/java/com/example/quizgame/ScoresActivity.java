9898178d4ba0d201a3b53e8225dbd95a737fe67d
alexander.siguenza <alexander.siguenza@gmail.com>
2021-06-06 03:09:46 -0600

    se agrego ScoresActivity

---
+++ app/src/main/java/com/example/quizgame/ScoresActivity.java
@@ -0,0 +1,33 @@
+package com.example.quizgame;
+
+import android.os.Bundle;
+import android.widget.RadioButton;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ScoresActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scores);
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radiomayor:
+                if (checked)
+                    Toast.makeText(getApplicationContext(),"ORDEN MAYOR",Toast.LENGTH_LONG).show();
+                    break;
+            case R.id.radiomenor:
+                if (checked)
+                    Toast.makeText(getApplicationContext(),"ORDEN MENOR",Toast.LENGTH_LONG).show();
+                    break;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/quizgame/adapter/ScoresAdapter.java
@@ -0,0 +1,5 @@
+package com.example.quizgame.adapter;
+
+public class ScoresAdapter {
+}
+
