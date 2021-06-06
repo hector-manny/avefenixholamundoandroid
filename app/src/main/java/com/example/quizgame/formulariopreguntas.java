d9e313ebead4d0f6e05ddf84073c7f867222cdcd
hector martinez <mejia.martinez2012@hotmail.es>
2021-06-06 12:24:01 -0600

    validaciones del formulario

--- app/src/main/java/com/example/quizgame/formulariopreguntas.java
+++ app/src/main/java/com/example/quizgame/formulariopreguntas.java
@@ -1,6 +1,8 @@
 package com.example.quizgame;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.view.View;
 import android.os.Bundle;
 import android.view.View;
@@ -43,29 +45,65 @@ public class formulariopreguntas extends AppCompatActivity {
         btnGuardar.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
-                int sum = Integer.parseInt(est1.getText().toString()) + Integer.parseInt(est2.getText().toString()) + Integer.parseInt(est3.getText().toString()) + Integer.parseInt(est4.getText().toString());
-                if(sum == 1){
-                    // insertar pregunta
-                    Pregunta pregunta = new Pregunta(etPregunta.getText().toString(),Integer.parseInt(etCantidadPun.getText().toString()));
-                    QuizLab.addPregunta(pregunta);
-                    //insertar opciones de respuesta
-                    List<Pregunta> preguntas=QuizLab.getPreguntas();
-                    Respuesta respuesta = new Respuesta(pregunta.getIdpregunta(),etOp1.getText().toString(),Integer.parseInt(est1.getText().toString()));
-                    QuizLab.addRespuesta(respuesta);
-                    respuesta = new Respuesta(pregunta.getIdpregunta(),etOp2.getText().toString(),Integer.parseInt(est2.getText().toString()));
-                    QuizLab.addRespuesta(respuesta);
-                    respuesta = new Respuesta(pregunta.getIdpregunta(),etOp3.getText().toString(),Integer.parseInt(est3.getText().toString()));
-                    QuizLab.addRespuesta(respuesta);
-                    respuesta = new Respuesta(pregunta.getIdpregunta(),etOp4.getText().toString(),Integer.parseInt(est4.getText().toString()));
-                    QuizLab.addRespuesta(respuesta);
-                    List<Respuesta> respuestas=QuizLab.getRespuestas();
-                    Toast.makeText(getApplicationContext(),"Pregunta Guardada Correctamente",Toast.LENGTH_LONG).show();
-                }else {
-                    Toast.makeText(getApplicationContext(),"Error solo puede haber un valor verdadero.",Toast.LENGTH_LONG).show();
+                /*validacion de los campos */
+                if(etPregunta.getText().toString().trim().isEmpty() || etPregunta.getText().toString().trim().length() == 0){
+                    Toast.makeText(getApplicationContext(),"El campo pregunta esta vacio",Toast.LENGTH_LONG).show();
                 }
-
+                if(etCantidadPun.getText().toString().trim().isEmpty() || etCantidadPun.getText().toString().trim().length() == 0){
+                    Toast.makeText(getApplicationContext(),"El campo cantidad puntos esta vacio",Toast.LENGTH_LONG).show();
+                }
+                if(etOp1.getText().toString().trim().isEmpty() || etOp1.getText().toString().trim().length() == 0
+                        || etOp2.getText().toString().trim().isEmpty() || etOp2.getText().toString().trim().length() == 0
+                        || etOp3.getText().toString().trim().isEmpty() || etOp3.getText().toString().trim().length() == 0
+                        || etOp4.getText().toString().trim().isEmpty() || etOp4.getText().toString().trim().length() == 0){
+                    Toast.makeText(getApplicationContext(),"Un campo Opción esta vacio",Toast.LENGTH_LONG).show();
+                }
+                if(est1.getText().toString().trim().isEmpty() || est1.getText().toString().trim().length() == 0
+                        || est2.getText().toString().trim().isEmpty() || est2.getText().toString().trim().length() == 0
+                        || est3.getText().toString().trim().isEmpty() || est3.getText().toString().trim().length() == 0
+                        || est4.getText().toString().trim().isEmpty() || est4.getText().toString().trim().length() == 0) {
+                    Toast.makeText(getApplicationContext(), "El campo verdadero ó falso esta vacio", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    int sum = Integer.parseInt(est1.getText().toString()) + Integer.parseInt(est2.getText().toString()) + Integer.parseInt(est3.getText().toString()) + Integer.parseInt(est4.getText().toString());
+                    if (sum == 1) {
+                        // insertar pregunta
+                        Pregunta pregunta = new Pregunta(etPregunta.getText().toString(), Integer.parseInt(etCantidadPun.getText().toString()));
+                        QuizLab.addPregunta(pregunta);
+                        //insertar opciones de respuesta
+                        List<Pregunta> preguntas = QuizLab.getPreguntas();
+                        Respuesta respuesta = new Respuesta(pregunta.getIdpregunta(), etOp1.getText().toString(), Integer.parseInt(est1.getText().toString()));
+                        QuizLab.addRespuesta(respuesta);
+                        respuesta = new Respuesta(pregunta.getIdpregunta(), etOp2.getText().toString(), Integer.parseInt(est2.getText().toString()));
+                        QuizLab.addRespuesta(respuesta);
+                        respuesta = new Respuesta(pregunta.getIdpregunta(), etOp3.getText().toString(), Integer.parseInt(est3.getText().toString()));
+                        QuizLab.addRespuesta(respuesta);
+                        respuesta = new Respuesta(pregunta.getIdpregunta(), etOp4.getText().toString(), Integer.parseInt(est4.getText().toString()));
+                        QuizLab.addRespuesta(respuesta);
+                        List<Respuesta> respuestas = QuizLab.getRespuestas();
+                        /*limpiamos los valores */
+                        etPregunta.setText("");
+                        etCantidadPun.setText("");
+                        etOp1.setText("");
+                        etOp2.setText("");
+                        etOp3.setText("");
+                        etOp4.setText("");
+                        est1.setText("");
+                        est2.setText("");
+                        est3.setText("");
+                        est4.setText("");
+                        Toast.makeText(getApplicationContext(), "Pregunta Guardada Correctamente", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Error solo puede haber un valor verdadero.", Toast.LENGTH_LONG).show();
+                    }
+                }
             }
         });
+    }
+
+    public void BotonIrAtras(View vista){
+        Intent intent= new Intent (this, pantallabienvenida.class);
+        startActivity(intent);
     }
 
 
---
+++ app/src/main/java/com/example/quizgame/juegoterminado.java
@@ -0,0 +1,18 @@
+package com.example.quizgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+
+public class juegoterminado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juegoterminado);
+    }
+
+
+}
